4f89aa72dd9913b0894aa837d8b87a43901a8b4c
Replit user <>
2023-01-23 13:33:44 +0000

    cargo

---
+++ code/target/debug/code.d
@@ -0,0 +1,1 @@
+/home/runner/rusticwars/code/target/debug/code: /home/runner/rusticwars/code/src/main.rs
---
+++ code/target/debug/deps/code-af637dc6984b2cc4.d
@@ -0,0 +1,5 @@
+/home/runner/rusticwars/code/target/debug/deps/code-af637dc6984b2cc4: src/main.rs
+
+/home/runner/rusticwars/code/target/debug/deps/code-af637dc6984b2cc4.d: src/main.rs
+
+src/main.rs:
